8137e1524ba358f873539593577e64d639aaeb9c
albimdkr <albimudakar04@gmail.com>
2024-02-15 20:30:56 +0700

    feat: finalization hero sectio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,21 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       fontFamily: {
         inter: ['Inter'],
       },
       colors: {
         primary: '#186F65',
+        dark: '#of172a',
       },
+      screens: {
+        '2xl': '1320px'
+      }
     },
   },
   plugins: [],
