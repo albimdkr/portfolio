c4932f9816067cfdd1f0d3b7d92d63e91a706f51
albimdkr <albimudakar04@gmail.com>
2024-02-16 10:03:11 +0700

    feat: add toggle mobile first without list

---
+++ public/assets/js/script.js
@@ -0,0 +1,7 @@
+// Hamburger
+const hambuger = document.querySelector('#hamburger');
+
+hambuger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active');
+    // alert('toggle detected!')
+});
